cb7fac395f7aba66d0a8411b26b2a549a8d28f89
mersanuzun <mersanuzun@gmail.com>
2015-07-02 19:51:51 +0300

    disconnection is handle and change owner of room if owner is left

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var Room = require("./Room.js");
 var User = require("./User.js");
 var express = require("express");
 var app = express();
+app.use(express.static('public'));
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var users = {};
@@ -9,13 +10,13 @@ var rooms = [];
 var userID = 1;
 var roomID = 1;
 var resultRoom;
-var port = 8686;
+var port = 4070 ;
 server.listen(port);
 console.log("Server is running at", port);
-
+app.use(express.static('public'));
 app.get("/", function(req, res){
   res.sendfile(__dirname + "/index.html");
-})
+});
 io.sockets.on("connection", function(socket){
   socket.on("create-user", function(userName, callback){
     var user = new User(userName, userID++, socket.id);
@@ -52,6 +53,9 @@ io.sockets.on("connection", function(socket){
     console.log(socket.roomID)
     io.sockets.in(socket.roomID).emit("new-message", {userName : users[socket.id].userName, data : message});
   })
+  socket.on("typing", function(){
+    socket.broadcast.to(socket.roomID).emit("some-typing", users[socket.id]);
+  })
   socket.on("leave-room", function(callback){
     leaveRoom(socket);
     updateRooms();
@@ -59,8 +63,18 @@ io.sockets.on("connection", function(socket){
   })
   function leaveRoom(){
     socket.leave(socket.roomID);
-    resultRoom = findRoom(socket.roomID)
-    resultRoom.removeUser(users[socket.id].userID)
+    var resultRoom = findRoom(socket.roomID)
+    if (users[socket.id].userID == resultRoom.owner.userID){
+      resultRoom.removeUser(users[socket.id].userID);
+      if (resultRoom.members.length > 0) resultRoom.changeOwner(resultRoom.members[0]);
+      else{
+        removeRoom(resultRoom);
+        console.log(rooms);
+      }
+    }else{
+      resultRoom.removeUser(users[socket.id].userID);
+    }
+    console.log("owner", resultRoom.owner);
     updateRoomMembers(resultRoom);
     io.sockets.in(socket.roomID).emit("left-message", users[socket.id].userName + " left the room<br>");
   }
@@ -73,10 +87,21 @@ io.sockets.on("connection", function(socket){
     }
     return resultRoom;
   }
+  function removeRoom(room){
+    for(var i=0; i < rooms.length; i++){
+      if (rooms[i].roomID == room.roomID){
+        rooms.splice(i,1);
+        break;
+      }
+    }
+  }
   function updateRoomMembers(room){
     io.sockets.in(socket.roomID).emit("room-members", room.members);
   }
   function updateRooms(){
     io.emit("rooms", rooms);
   }
+  socket.on("disconnect", function(){
+    leaveRoom();
+  })
 })
